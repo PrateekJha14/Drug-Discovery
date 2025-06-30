223b3cbf169061207452544a8f23b069c390e268
Prateek <139892883+PrateekJha14@users.noreply.github.com>
2025-06-30 14:12:15 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://molecule-ai.onrender.com"],  # ✅ specific origin
+    allow_origins=["https://drug-discovery-ai-ui.onrender.com/"],  # ✅ specific origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
